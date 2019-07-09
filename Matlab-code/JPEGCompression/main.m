f64e269383cf77565b19e9c1d9b462ac7d841804
Jiaming Wang <54988890@qq.com>
2019-07-09 15:16:29 +0800

    Update

--- Matlab-code/JPEGCompression/main.m
+++
@@ -1,8 +0,0 @@
-f=imread('d.tif');
-f=modcrop(f,8);
-%                            quality
-[output]= jpegCompression(f,10);
-figure;
-subplot(1,2,1),imshow(f);
-imwrite(uint8(output),'q10.png','png');
-subplot(1,2,2),imshow(output);
